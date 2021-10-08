2bb0eae42b1604bf46d004e28ac8bda5cf8b30e2
vanishima <shushu@nyu.edu>
2021-10-08 15:26:00 -0700

    final changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,21 @@
 // alert("Alert Test");
 
-var contentStr = ["resumeDiv", "deskDiv", "contactsDiv"];
+var contentStr = ["resumeSect", "deskSect", "contactsSect"];
 
 // show ONLY the content toShow, hide all other contents
 function navClick(toShow) {
-	contentStr.forEach(function(name) {
-		// get div element
-		var content = document.getElementById(name);
-		if (name == toShow){
-			content.style.display = "block";	// show
-		} else {
-			content.style.display = "none";		// hide
-		}
-	});
+    contentStr.forEach(function (name) {
+        // get div element
+        var content = document.getElementById(name);
+        if (name == toShow) {
+            content.style.display = "block"; // show only this
+        } else {
+            content.style.display = "none"; // hide all others
+        }
+    });
+
+    // if contacts Div is selected, change background image
+    if (toShow == "contactsSect"){
+        document.body.style.backgroundImage = "linear-gradient(to right, rgba(0, 0, 0, 0.6) 0 100%), url(\"/images/home-page-1.jpg\")";
+    }
 }
