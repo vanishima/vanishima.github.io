12b09d03e83171b21388d3646fd90d5fa17c03cc
vanishima <shushu@nyu.edu>
2021-10-10 12:48:00 -0700

    add button for resume, create new pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,15 @@
 // alert("Alert Test");
 
-var contentStr = ["resumeSect", "deskSect", "contactsSect"];
+var resumeBtns = ["btncheckEdu", "btncheckExp", "btncheckSki", "btncheckPro"];
 
-// show ONLY the content toShow, hide all other contents
-function navClick(toShow) {
-    contentStr.forEach(function (name) {
-        // get div element
-        var content = document.getElementById(name);
-        if (name == toShow) {
-            content.style.display = "block"; // show only this
-        } else {
-            content.style.display = "none"; // hide all others
-        }
-    });
-
-    // if contacts Div is selected, change background image
-    if (toShow == "contactsSect") {
-        document.body.style.backgroundImage =
-            "linear-gradient(to right, rgba(0, 0, 0, 0.2) 0 100%), url(\"/images/background/home-page-1.jpg\")";
-    }
+function resumeCheck(sectName){
+    console.log(sectName, " clicked");
+    resumeBtns.forEach(function (btnName){
+        // get button
+        var button = document.getElementById(btnName);
+        // show the corresponding section
+        var sect = document.getElementById(sectName);
+        // show with each click
+        sect.style.display = sect.style.display === "none" ? "block" : "none";
+    })
 }
