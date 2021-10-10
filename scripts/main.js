31abf68ce3992e9019267f2d9adb91df79583ea3
vanishima <shushu@nyu.edu>
2021-10-10 15:34:34 -0700

    revised javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,26 @@
-// alert("Alert Test");
+var btnChecked = {
+  btncheckEdu: false,
+  btncheckExp: false,
+  btncheckSki: false,
+  btncheckPro: false,
+};
+var sectToBtn = {
+  educationSect: "btncheckEdu",
+  experienceSect: "btncheckExp",
+  skillsSect: "btncheckSki",
+  projectsSect: "btncheckPro",
+};
 
-var resumeBtns = ["btncheckEdu", "btncheckExp", "btncheckSki", "btncheckPro"];
+function resumeCheck(clickedBtnName) {
+  btnChecked[clickedBtnName] =
+    btnChecked[clickedBtnName] === true ? false : true;
 
-function resumeCheck(sectName){
-    console.log(sectName, " clicked");
-    resumeBtns.forEach(function (btnName){
-        // get button
-        var button = document.getElementById(btnName);
-        // show the corresponding section
-        var sect = document.getElementById(sectName);
-        // show with each click
-        sect.style.display = sect.style.display === "none" ? "block" : "none";
-    })
+  /* Iterate through each section */
+  Object.keys(sectToBtn).forEach(function (resumeSect) {
+    // get the corresponding section
+    var sect = document.getElementById(resumeSect);
+    // show when checked, hide when unchecked
+    sect.style.display =
+      btnChecked[sectToBtn[resumeSect]] === true ? "block" : "none";
+  });
 }
